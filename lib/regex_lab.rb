5121ac03f8e3ea2e2ea0b667109425359996080a
michaelstsnow <michael.st.snow@gmail.com>
2018-11-13 20:25:19 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,15 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  all_words=text.search(/\w/)
-  words_with_un_ing=[]
-  all_words.each do |word|
-    if word.search(/\A+un/) && word.search(/\Z+ing/)
-      words_with_un_ing << word
-    else
-    end
-  end #End of do word
-  return words_with_un_ing
+  text.scan(/\b(un)[a-z](ing)\b/)
 end
 
 def words_five_letters_long(text)
